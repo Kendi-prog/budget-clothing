5160d3ae960270a317af57395672f0edf79d4ad4
Joy Leila Kendi <muriungikendileila@gmail.com>
2024-11-19 09:19:26 +0300

    created the sign up, form input, and button components

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -3,7 +3,9 @@ import {
     getAuth,
     signInWithRedirect,
     signInWithPopup,
-    GoogleAuthProvider
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
 }
 from "firebase/auth";
 
@@ -29,18 +31,21 @@ const firebaseConfig = {
 
 const FirebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDatabaseFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    if(!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid);
 
     console.log(userDocRef);
@@ -58,7 +63,8 @@ export const createUserDatabaseFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation,
             });
         } catch(error){
           console.log('Error created the user', error.message)
@@ -66,6 +72,11 @@ export const createUserDatabaseFromAuth = async (userAuth) => {
     }
     return userDocRef; 
     
-
     
+}
+
+export const createAuthUserWithEmailAndPassword = async ( email, password) => {
+    if(!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
 }
