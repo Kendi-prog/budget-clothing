c177a5889118c1728554decb6a222e16c5d49f69
Joy Leila Kendi <muriungikendileila@gmail.com>
2024-12-17 15:29:40 +0300

    modifying my code by adding redux thunk and adding a spinner for the page when loading

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,14 +1,16 @@
 import { useParams } from 'react-router-dom';
 import { Fragment, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 import ProductCard from '../../components/product-card/product-card.component';
 import { CategoryContainer, CategoryTitle } from './category.styles';
+import Spinner from '../../components/spinner/spinner.component';
 
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [ products, setProducts ] = useState(categoriesMap[category]);
 
     useEffect(() => {
@@ -18,9 +20,18 @@ const Category = () => {
     return(
         <Fragment>
              <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-             <CategoryContainer>
-                {products && products.map((product) => <ProductCard  key={products.id} product={product} />)}
-            </CategoryContainer>
+             {
+                isLoading ? (
+                    <Spinner />
+                ) : (
+                    <CategoryContainer>
+                        {products && 
+                            products.map((product) => (
+                                <ProductCard  key={product.id} product={product} />))}
+                    </CategoryContainer>
+                )
+             }
+            
         </Fragment>
           
     );
--- src/store/store.js
+++ src/store/store.js
@@ -4,18 +4,19 @@ import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
 import storage from 'redux-persist/lib/storage';
 import logger from "redux-logger";
+import { thunk } from "redux-thunk";
 import { rootReducer } from "./root-reducer";
 
 
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user'],
+    whitelist: ['cart'],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 const composeEnhancer = (
     process.env.NODE_ENV !== 'production' && 
